4cdc2f03bae38d3f55a55f2b774fb14249de5dac
Piyushnmj <piyushnmj@yahoo.com>
2022-10-19 00:09:02 +0530

    Fibonacci Series

--- LogicalProblems/LogicalProblems/Program.cs
+++ LogicalProblems/LogicalProblems/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace LogicalProblems
+﻿class FinonacciSeries
 {
-    internal class Program
+    public static int Fibonacci(int num)
     {
-        static void Main(string[] args)
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i < num; i++)
         {
-            Console.WriteLine("Hello, World!");
+            int temp = a;
+            a = b;
+            b = temp + b;
         }
+        return a;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter N terms for Fibonacci Series: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The Fibonacci Series of {0} terms are:", N);
+
+        for (int i = 0; i < N; i++)
+        {
+            Console.WriteLine("{0}" , Fibonacci(i));
+        }
+        Console.ReadLine();
     }
 }
